5ac962835f46c817facf346dba8ec0b2fc86143f
timothy <tsingletary1579@gmail.com>
2024-01-04 11:14:42 -0600

    cleaning it up

--- ShoppingList.js
+++ ShoppingList.js
@@ -1,39 +1,3 @@
-//
-// $(function() {
-//     let callback = function(event) {
-//         event.preventDefault();
-//         let input = $('input[type=text][name=item]'),
-//
-//             value = input.val(),
-//             need = ($(event.target).attr('id') === 'addNeed'),
-//
-//             item = $('<li><input type="checkbox" name="item"> ' + value + ' <a href="#">remove</a><li>'),
-//
-//             list = (need) ? $('ul').first() : $('ul').last();
-//
-//         input.val("");
-//         input.focus();
-//
-//         if (value === "") return;
-//
-//         if (!need) {
-//             item.find('input').attr('checked', true);
-//         }
-//
-//         $('#addHave, #addNeed').click(callback);
-//
-//         $('ul').on('click', 'li a', function (event) {
-//             $(event.target).parent('li').remove();
-//         });
-//
-//         $('ul').on('click', 'input[type=checkbox]', function (event) {
-//             let listItem = $(event.target).parent('li'),
-//                 list = (event.target.checked) ? $('ul').last() : $('ul').first();
-//             listItem.appendTo(list);
-//         });
-//
-//     }});
-
 $(function() {
     let callback = function(event) {
         event.preventDefault();
